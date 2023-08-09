2947846356e4013456027ed430d16d5555675984
faisalalm01 <faisalali2858@gmail.com>
2023-08-09 07:24:57 +0700

    add login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,6 +2,7 @@ const authRoutes = require('express').Router();
 const authControllers = require('../controllers/authControllers');
 
 authRoutes.post('/register', authControllers.register);
-authRoutes.get('/verify/:token', authControllers.verify)
+authRoutes.get('/verify/:token', authControllers.verify);
+authRoutes.get('/login', authControllers.login);
 
 module.exports = authRoutes
