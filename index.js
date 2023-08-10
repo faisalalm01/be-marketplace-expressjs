0dc0bd0d626fe31ceff7de5a86e8aae0aed9ece4
muhammadisa226 <muhammadisa226@gmail.com>
2023-08-10 18:15:08 +0700

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-require('dotenv').config({})
-const express = require('express')
-const app = express()
+require("dotenv").config({});
+const express = require("express");
+const app = express();
 const port = process.env.PORT;
-const mainRoutes = require('./src/routes')
+const mainRoutes = require("./src/routes");
 
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 // app.set('view engine', 'ejs');
-app.use('/api', mainRoutes)
-
+app.use("/api", mainRoutes);
 
 // xample
-app.get('/api', async (req, res) => {
-    res.send({message: 'api its works 🐻'})
-})
+app.get("/api", async (req, res) => {
+  res.send({ message: "api its works 🐻" });
+});
 
 // port
-app.listen(port, () => console.log(`server run on http://localhost:${port}`))
+app.listen(port, () => console.log(`server run on http://localhost:${port}`));
