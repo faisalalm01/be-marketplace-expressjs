612ceddbdc5f36c1673675fc56be6d9804c5910c
faisalalm01 <faisalali2858@gmail.com>
2023-08-12 16:36:51 +0700

    add get market and edit market

--- src/controllers/marketControllers.js
+++ src/controllers/marketControllers.js
@@ -28,6 +28,47 @@ module.exports = {
                 error
             })
         })
+    },
+
+    getAllMarket : (req, res) => {
+        market.findAll()
+        .then((data) => {
+            res.status(200).send({
+                msg: 'success get all data',
+                status: 200,
+                data
+            })
+        })
+        .catch((error) => {
+            res.status(500).send({
+                msg: 'failed get all data',
+                status: 500,
+                error
+            })
+        })
+    },
+
+    editMarket : (req, res) => {
+        const {body} = req;
+        const {id} = req.params;
+
+        market.update(body, {
+            where: {id}
+        })
+        .then((data) => {
+            res.status(200).send({
+                msg: 'success update data',
+                status: 200,
+                data
+            })
+        })
+        .catch((error) => {
+            res.status(500).send({
+                msg: 'failed update data',
+                status: 500,
+                error
+            })
+        })
     }
 
 }
--- src/routes/marketRoutes.js
+++ src/routes/marketRoutes.js
@@ -3,5 +3,7 @@ const marketControllers = require('../controllers/marketControllers');
 const authMiddleware = require('../helpers/middleware/authMiddleware')
 
 marketRoutes.post('/', authMiddleware.checkLogin, marketControllers.createMarket);
+marketRoutes.get('/', marketControllers.getAllMarket);
+marketRoutes.put('/:id', marketControllers.editMarket)
 
 module.exports = marketRoutes
