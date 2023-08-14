bb81db57fe54795ab85bf4827198f177b347c6a3
faisalalm01 <faisalali2858@gmail.com>
2023-08-14 14:25:06 +0700

    init project

--- src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const userRoutes = require('express').Router();
-const userControllers = require('../controllers/userControllers');
-const authMiddleware = require('../helpers/middleware/authMiddleware')
-
-
-userRoutes.get('/', authMiddleware.checkLogin, userControllers.getDataUser);
-userRoutes.get('/product', authMiddleware.checkLogin, userControllers.getUserProduct);
-
-module.exports = userRoutes
