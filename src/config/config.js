5dafc87c21385a0b859370e6fb959a9eb8e588f8
faisalalm01 <faisalali2858@gmail.com>
2023-08-18 07:46:27 +0700

    update config

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,10 @@
-{
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "marketplace",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DB,
+    "host": process.env.HOST,
+    "dialect": process.env.DIALECT
   },
   "test": {
     "username": "root",
